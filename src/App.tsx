bbadb8df642487f78cfabb08a6143bb698bebdc8
wooffet <wooffet@gmail.com>
2021-11-24 00:18:37 +0000

    Styling updates + lazy-load logic
    Update title font + text - Slmpson/wooffet
    Add nice, subtle bg colour - Slmpson
    Add native lazy-loading + fallback to use lazysizes package - wooffet
    Add FeedImage component - wooffet
    WIP placeholder images - wooffet

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { useEffect, useRef, useState } from 'react';
+import lazysizes from 'lazysizes';
 import './App.css';
 
 const App = () => {
+  const [useLazysizes, setUseLazysizes] = useState(false);
+
+  lazysizes.cfg.init = false;
+
+  useEffect(() => {
+    if ('loading' in HTMLImageElement.prototype) {
+      setUseLazysizes(false);
+    } else {
+      setUseLazysizes(true);
+    }
+  }, []);
+
   const getImages = () => {
-    let images = [];
+    let images: string[] = [];
     const size = 300;
     const rows = 10;
     for (let i = 0; i < rows; i++) {
@@ -19,13 +33,51 @@ const App = () => {
 
   return (
     <>
-      <h1 className='title'>Unsplash Image Feed</h1>
-      <h2 className='title'>Instagram-like feed of images :)</h2>
+      <h1 className='title'>Unsplash Image Feed 😀</h1>
+      <h2 className='sub-title'>
+        Instagram-like feed of images, made in React (☞ﾟヮﾟ)☞
+      </h2>
       <div className='feed-container'>
-        {getImages().map((image, index) => {
-          return <img key={index} src={image} alt='' />;
-        })}
+        {getImages().map((image, index) => (
+          <FeedImage key={index} src={image} isLazysize={useLazysizes} />
+        ))}
       </div>
+    </>
+  );
+};
+
+const FeedImage = (props: { src: string; isLazysize: boolean }) => {
+  const [usePlaceholder, setUsePlaceholder] = useState(false);
+  const { src, isLazysize } = props;
+  const imgElementRef = useRef();
+
+  useEffect(() => {
+    const imgElement: HTMLImageElement | undefined = imgElementRef.current;
+    if (imgElement) {
+      setUsePlaceholder(imgElement.complete);
+    } else {
+      setUsePlaceholder(false);
+    }
+  }, []);
+
+  return (
+    <>
+      {isLazysize ? (
+        <img
+          ref={imgElementRef}
+          data-src={usePlaceholder ? 'placeholderrrrrrr!' : src}
+          alt=''
+          loading='lazy'
+          className='lazyload'
+        />
+      ) : (
+        <img
+          ref={imgElementRef}
+          src={usePlaceholder ? 'placeholderrrrrrr!' : src}
+          alt=''
+          loading='lazy'
+        />
+      )}
     </>
   );
 };
